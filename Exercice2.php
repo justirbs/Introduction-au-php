c4034477aad0b693ee8601edf093a00db8bd065d
Justine <ribasjusti@eisti.eu>
2021-03-22 14:58:16 +0100

    exo 2 terminé

--- Exercice2.php
+++ Exercice2.php
@@ -55,17 +55,6 @@
     }
   ?>
 
-  <?php
-    // on initie le tableau avec les valeurs aléatoires
-    $tab = initTab();
-    echo("<p>Tableau non trié</p>");
-    afficher($tab);
-    echo("<p>Tableau trié</p>");
-    // on trie le tableau
-    $tab = triABulle($tab);
-    afficher($tab);
-   ?>
-
 
 
 
